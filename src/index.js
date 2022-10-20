6fe51ba23e7d7b2d488ecb734257538f2164344f
x3iiDes <x3iidestiny@hotmail.sg>
2022-10-20 21:35:49 +0800

    upgrade npm to latest version, remove bootstrap dependencies and add tailwind script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from './App';
